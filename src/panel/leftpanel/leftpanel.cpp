f79ce83a4aaacf46e8961197634b754fd1a90fce
Sapronov Alex <warmonger72@gmail.com>
2012-12-13 23:01:52 +0700

    [UPDATE] update //------ strings

--- src/panel/leftpanel/leftpanel.cpp
+++ src/panel/leftpanel/leftpanel.cpp
@@ -13,54 +13,54 @@ LeftPanel::LeftPanel(QWidget *parent, MainWindow *h):
 {
     ui->setupUi(this);
 }
-//-------------------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
 LeftPanel::~LeftPanel()
 {
     delete ui;
 }
-//-------------------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
 void LeftPanel::loadProject(QString file)
 {
     parseProjectFile(file);
     filename = file;
-//    debug();
+    debug();
 }
-//-------------------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
 void LeftPanel::debug()
 {
     qDebug() << "Activate debug: leftPanel";
 
-//    standardModel = new QStandardItemModel ;
+    standardModel = new QStandardItemModel ;
 //    standardModel = parseToc(&getTextFromFile(filename)) ;
-//    QStandardItem *rootNode = standardModel->invisibleRootItem();
+    QStandardItem *rootNode = standardModel->invisibleRootItem();
 
-//    //defining a couple of items
-//    QStandardItem *americaItem = new QStandardItem("America");
-//    QStandardItem *mexicoItem =  new QStandardItem("Canada");
-//    QStandardItem *usaItem =     new QStandardItem("USA");
-//    QStandardItem *bostonItem =  new QStandardItem("Boston");
-//    QStandardItem *europeItem =  new QStandardItem("Europe");
-//    QStandardItem *italyItem =   new QStandardItem("Italy");
-//    QStandardItem *romeItem =    new QStandardItem("Rome");
-//    QStandardItem *veronaItem =  new QStandardItem("Verona");
+    //defining a couple of items
+    QStandardItem *americaItem = new QStandardItem("America");
+    QStandardItem *mexicoItem =  new QStandardItem("Canada");
+    QStandardItem *usaItem =     new QStandardItem("USA");
+    QStandardItem *bostonItem =  new QStandardItem("Boston");
+    QStandardItem *europeItem =  new QStandardItem("Europe");
+    QStandardItem *italyItem =   new QStandardItem("Italy");
+    QStandardItem *romeItem =    new QStandardItem("Rome");
+    QStandardItem *veronaItem =  new QStandardItem("Verona");
 
-//    //building up the hierarchy
-//    rootNode->    appendRow(americaItem);
-//    rootNode->    appendRow(europeItem);
-//    americaItem-> appendRow(mexicoItem);
-//    americaItem-> appendRow(usaItem);
-//    usaItem->     appendRow(bostonItem);
-//    europeItem->  appendRow(italyItem);
-//    italyItem->   appendRow(romeItem);
-//    italyItem->   appendRow(veronaItem);
+    //building up the hierarchy
+    rootNode->    appendRow(americaItem);
+    rootNode->    appendRow(europeItem);
+    americaItem-> appendRow(mexicoItem);
+    americaItem-> appendRow(usaItem);
+    usaItem->     appendRow(bostonItem);
+    europeItem->  appendRow(italyItem);
+    italyItem->   appendRow(romeItem);
+    italyItem->   appendRow(veronaItem);
 
     //register the model
     ui->treeViewProject->setModel(standardModel);
     ui->treeViewProject->expandAll();
 
 }
-//-------------------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
 
-//-------------------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
 
-//-------------------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
